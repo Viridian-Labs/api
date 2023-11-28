f421b7ac9d7b8f217df794d0db232c9e3942aafa
diego <pythinking@gmail.com>
2023-11-28 15:11:22 -0300

    Changed the script to PEP8 compatible style

--- scripts/compare_assets.py
+++ scripts/compare_assets.py
@@ -1,29 +1,29 @@
 import requests
 
-# Fetching data from the APIs
-json1_data = requests.get(
-    "https://api.equilibrefinance.com/api/v1/assets"
-).json()["data"]
-json2_data = requests.get("http://localhost:8000/api/v1/assets").json()["data"]
 
-# Function to transform data into a dictionary with address as key
 def transform_data(data):
+    """Transform data into a dictionary with address as key."""
     return {token["address"]: token for token in data}
 
 
-# Function to check if prices are close based on percentual difference
 def are_prices_close(price1, price2, percent_tolerance=0.05):
+    """Check if prices are close based on percentual difference."""
     if price1 == 0 or price2 == 0:
         return False  # Avoid division by zero
     percent_difference = abs(price1 - price2) / max(price1, price2)
     return percent_difference <= percent_tolerance
 
 
-# Transforming the data sets
+# Fetching data from the APIs
+json1_url = "https://api.equilibrefinance.com/api/v1/assets"
+json1_data = requests.get(json1_url).json()["data"]
+
+json2_url = "http://localhost:8000/api/v1/assets"
+json2_data = requests.get(json2_url).json()["data"]
+
 tokens1 = transform_data(json1_data)
 tokens2 = transform_data(json2_data)
 
-# Comparing the prices
 different_prices = []
 same_prices = []
 close_prices = []
@@ -42,20 +42,15 @@ for address, token1 in tokens1.items():
         else:
             same_prices.append((symbol, address, price1))
 
-# Displaying the results
 print("Tokens with different prices:")
 for symbol, address, price1, price2 in different_prices:
-    print(
-        f"Symbol: {symbol}, Address: {address}, "
-        f"Price in Prod: {price1}, Price in Stag: {price2}"
-    )
+    print(f"Symbol: {symbol}, Address: {address}, "
+          f"Price in Prod: {price1}, Price in Stag: {price2}")
 
 print("\nTokens with close and acceptable prices:")
 for symbol, address, price1, price2 in close_prices:
-    print(
-        f"Symbol: {symbol}, Address: {address}, "
-        f"Price in Prod: {price1}, Price in Stag: {price2}"
-    )
+    print(f"Symbol: {symbol}, Address: {address}, "
+          f"Price in Prod: {price1}, Price in Stag: {price2}")
 
 print("\nTokens with the same price:")
 for symbol, address, price in same_prices:
